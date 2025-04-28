ecb678baaefbd3d095f9985ad7febec968eb4eda
SlavaVasilyevQA <thenovalive@gmail.com>
2025-04-28 18:06:22 +0300

    Модифицировал тесты, а именно раздел "list resource" переместил в новый файл "test_get_list_resource.py"

--- tests/test_get_user_data.py
+++ tests/test_get_user_data.py
@@ -1,20 +1,18 @@
 import httpx
 from jsonschema import validate
-from core.contracts import USER_DATA_SCHEME, RESOURCE_DATA_SCHEME
+from core.contracts import USER_DATA_SCHEME
 
 BASE_URL = "https://reqres.in"
 LIST_USERS = "/api/users?page=2"
-EMAIL_ENDS = "@reqres.in"
-AVATAR_ENDS = "-image.jpg"
 SINGLE_USER = "/api/users/2"
 SINGLE_USER_NOT_FOUND = "/api/users/23"
-LIST_RESOURCE = "/api/unknown"
-SINGLE_RESOURCE = "/api/unknown/2"
-SINGLE_RESOURCE_NOT_FOUND = "/api/unknown/23"
+EMAIL_ENDS = "@reqres.in"
+AVATAR_ENDS = "-image.jpg"
 
 headers = {
     "x-api-key": "reqres-free-v1"
 }
+
 
 def test_list_users():
     response = httpx.get(BASE_URL + LIST_USERS, headers=headers)
@@ -38,23 +36,4 @@ def test_single_user():
 
 def test_user_not_found():
     response = httpx.get(BASE_URL + SINGLE_USER_NOT_FOUND, headers=headers)
-    assert response.status_code == 404
-
-
-def test_list_resource():
-    response = httpx.get(BASE_URL + LIST_RESOURCE, headers=headers)
-    assert response.status_code == 200
-    data = response.json()["data"]
-
-    for item in data:
-        validate(item, RESOURCE_DATA_SCHEME)
-
-def test_single_resource():
-    response = httpx.get(BASE_URL + SINGLE_RESOURCE, headers=headers)
-    assert response.status_code == 200
-    data = response.json()["data"]
-    validate(data, RESOURCE_DATA_SCHEME)
-
-def test_single_resource_not_found():
-    response = httpx.get(BASE_URL + SINGLE_RESOURCE_NOT_FOUND, headers=headers)
     assert response.status_code == 404
